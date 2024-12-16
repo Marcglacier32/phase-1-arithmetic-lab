0af13dcbfb19d016afd26fc987a7428f2eb4d76c
Mark Thiongo <mark.thiongo@student.moringaschool.com>
2024-12-16 19:17:42 +0300

    Completed arithmetic lab challenges

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Challenge 1: Multiply two numbers
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result should be 62
+
+// Challenge 2: Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; // Random number greater than 0
+
+// Challenge 3: Calculate the remainder
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // Remainder should be 4
+
+// Challenge 4: Find the maximum number
+let max = Math.max(1, 5, 10, 20); // Maximum number in the set
